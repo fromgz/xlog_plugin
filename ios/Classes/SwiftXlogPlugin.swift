4a62aa944d904912be125c02870864b8d837a24e
will lan <228669797@qq.com>
2020-03-24 18:13:32 +0800

    ios修改

--- ios/Classes/SwiftXlogPlugin.swift
+++ ios/Classes/SwiftXlogPlugin.swift
@@ -7,18 +7,23 @@ public class SwiftXlogPlugin: NSObject, FlutterPlugin {
     let channel = FlutterMethodChannel(name: "xlog_plugin", binaryMessenger: registrar.messenger())
     let instance = SwiftXlogPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    
+    DDLog.add(DDTTYLogger.sharedInstance!)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     //result("iOS " + UIDevice.current.systemVersion)
-    let args = call.arguments as! [String: Any];
+    let args = call.arguments as? Dictionary<String, String>
+    
     switch call.method {
-        case "getPlatformVersion":
-        case "logD":
-        case "logW":
-        case "logI":
-        case "logE":
-        case "logJson":
+    case "getPlatformVersion":
+        result("iOS " + UIDevice.current.systemVersion);
+        break;
+    case "logD": DDLogDebug(args?["msg"] ?? ""); break;
+    case "logW": DDLogWarn(args?["msg"] ?? ""); break;
+    case "logI": DDLogInfo(args?["msg"] ?? ""); break;
+    case "logE": DDLogError(args?["msg"] ?? ""); break;
+    case "logJson": DDLogDebug(args?["msg"] ?? ""); break;
         default:
             result(FlutterMethodNotImplemented)
     }
